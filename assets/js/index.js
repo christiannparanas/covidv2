577d8054d249b9aa1f85635a53d5b2e3c13553ae
Christian Paranas <christiannparanas@gmail.com>
2020-06-24 22:06:10 +0800

    some stats

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,6 +4,7 @@ const global_cases = document.querySelector('#cases .value')
 const global_active = document.querySelector('#active .value')
 const global_recovered = document.querySelector('#recovered .value')
 const global_deaths = document.querySelector('#deaths .value')
+const date = document.querySelector('.date')
 
 
 // global fetch data
@@ -34,10 +35,36 @@ globalData()
 function globalStats(data) {
    
    global_cases.innerHTML = numberWithCommas(data.Global.TotalConfirmed);
+   global_active.innerHTML = numberWithCommas(data.Global.TotalConfirmed - data.Global.TotalRecovered - data.Global.TotalDeaths);
    global_recovered.innerHTML = numberWithCommas(data.Global.TotalRecovered);
+   global_deaths.innerHTML = numberWithCommas(data.Global.TotalDeaths);
+   date.innerHTML = `Last Updated: ${convertDate(data.Date)}`
 }
 
 // comma separator number
 function numberWithCommas(x) {
    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+// date converter
+function convertDate(str) {
+   var date = new Date(str),
+     mnth = ("" + (date.getMonth() + 1)).slice(-2),
+     day = ("" + date.getDate()).slice(-2) + ",";
+
+     let months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+     }
+   return [months[mnth], day, date.getFullYear()].join(" ");
 }
