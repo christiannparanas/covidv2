2bd08fbbe86a8472fcf915bab7899dea1dd6d6d1
Christian Paranas <christiannparanas@gmail.com>
2020-06-28 15:48:55 +0800

    fix search feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,6 +8,9 @@ const date = document.querySelector('.date')
 
 const countriesPlace = document.querySelector('.countries')
 
+let bansa = [];
+
+
 
 // fetch data
 function Data() {
@@ -24,20 +27,25 @@ function Data() {
       }
    })
    .then( data => {
-      
+      console.log(data)
+      let id = 0;
 
       let keys = Object.keys(data.Countries)
+      let DATA = data.Countries;
       
       keys.forEach( key => {
-         countriesPlace.innerHTML += 
-         `<div class="container">
-            <div class="country-name">${data.Countries[key].Country}</div>
-            <div class="country-cases">Total Cases: ${numberWithCommas(data.Countries[key].TotalConfirmed)}</div>
-            <div class="country-recovered">Recovered: ${numberWithCommas(data.Countries[key].TotalRecovered)}</div>
-            <div class="country-deaths">Deaths: ${numberWithCommas(data.Countries[key].TotalDeaths)}</div>
-         </div>`
-      })
+         bansa.push(DATA[key].Country)
 
+         countriesPlace.innerHTML += 
+         `<div class="container" id="bansa${id}">
+            <div class="country-name">${DATA[key].Country}</div>
+            <div class="country-cases">Total Cases: ${numberWithCommas(DATA[key].TotalConfirmed)}</div>
+            <div class="country-recovered">Recovered: ${numberWithCommas(DATA[key].TotalRecovered)}</div>
+            <div class="country-deaths">Deaths: ${numberWithCommas(DATA[key].TotalDeaths)}</div>
+         </div>`
+
+         id++;
+      })
 
       globalStats(data)
    })
@@ -57,4 +65,20 @@ function globalStats(data) {
    date.innerHTML = `Last Updated: ${convertDate(data.Date)}`
 }
 
-// show countries
+// search country
+const inpu = document.getElementById('input');
+
+inpu.addEventListener('input', () => {
+   let value = inpu.value.toUpperCase();
+   let num = 0;
+
+   bansa.forEach( bans => {
+      if(bans.toUpperCase().startsWith(value)){
+         document.getElementById(`bansa${num}`).classList.remove("hide")
+      } else {
+         document.getElementById(`bansa${num}`).classList.add("hide")
+      }
+
+      num++;
+   })
+})
